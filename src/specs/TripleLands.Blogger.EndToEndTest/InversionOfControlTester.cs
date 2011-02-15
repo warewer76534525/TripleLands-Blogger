bff52455f2592301c82ff62f71ad888f1e0cf870
Adi Sembiring <sembiring.adi@gmail.com>
2011-02-15 23:28:45 +0700

    set app config for spring

---
+++ src/specs/TripleLands.Blogger.EndToEndTest/InversionOfControlTester.cs
@@ -0,0 +1,18 @@
+﻿
+using NUnit.Framework;
+using Spring.Context.Support;
+using Spring.Context;
+using TripleLands.Blogger.EndToEndTest.springcomponent;
+namespace TripleLands.Blogger.EndToEndTest
+{
+    [TestFixture]
+    class InversionOfControlTester
+    {
+        [Test]
+        public void TestObjectContext() {
+            IApplicationContext context = ContextRegistry.GetContext();
+            Calculator c = (Calculator) context.GetObject("Calculator");
+            Assert.AreEqual(4, c.Add(2, 2));
+        }
+    }
+}
---
+++ src/specs/TripleLands.Blogger.EndToEndTest/springcomponent/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TripleLands.Blogger.EndToEndTest.springcomponent
+{
+    class Calculator
+    {
+        public int Add(int a, int b) {
+            return a + b;
+        }
+    }
+}
