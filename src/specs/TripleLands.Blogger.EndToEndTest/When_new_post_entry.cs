8f70721c0f3a4dcd397abcd19a48f738bf9c4730
Adi Sembiring <sembiring.adi@gmail.com>
2011-02-15 23:28:05 +0700

    add spring framework injection

--- src/specs/TripleLands.Blogger.EndToEndTest/When_new_post_entry.cs
+++ src/specs/TripleLands.Blogger.EndToEndTest/When_new_post_entry.cs
@@ -1,16 +1,26 @@
 ﻿using NUnit.Framework;
 using TripleLands.Blogger.Domain;
 using TripleLands.Blogger.Service;
+using Spring.Context.Support;
+using Spring.Context;
 
 namespace TripleLands.Blogger.EndToEndTest
 {
     [TestFixture]
     class When_new_post_entry
     {
+        IApplicationContext context;
+        IBlogService blogService;
+
+        [SetUp]
+        public void SetUp() {
+            context = ContextRegistry.GetContext();
+            blogService = (IBlogService) context.GetObject("BlogService");
+        }
+
         [Test]
         public void Should_display_in_recents_entry() {
             Post post = new Post("title", "content", "author");
-            IBlogService blogService = new BlogService();
             blogService.Post(post);
 
             Assert.AreEqual(post, blogService.GetRecentPost());
